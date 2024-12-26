70fd814387da29d4f8efd8ace5d78d023115ca28
reyzix444 <149589918+reyzix444@users.noreply.github.com>
2024-12-27 00:48:08 +0100

    Update and rename .gitignore to Sfd.php

---
+++ Sfd.php
@@ -0,0 +1,10 @@
+
+{
+  "status" : "heheheboy",
+  "ip" : "2a02:aee3f:aaaa:c101:2993:rr3c:dc87:0000, 000.000.000.000",
+  "time" : "2024-12-26 23:26:03",
+  "key" : "REYZIX LHWAY",
+  "UUID" : "2DE30CF2-D125-4E60-8735-FC05852A89B0",
+  "agent" : "Free%20Fire\/2019117860 CFNetwork\/1399 Darwin\/22.1.0",
+  "amount" : "99999999999 REYZIX"
+}
